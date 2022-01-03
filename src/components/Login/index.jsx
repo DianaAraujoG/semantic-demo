b97a1c80a23ff9f9ea059f11fdef3f5ee631a4bd
DianaAraujoG <dianac251@gmail.com>
2022-01-03 14:47:01 -0600

    UPDATE(Login): Message

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,12 +1,12 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
-import { Header, Form, Grid, Segment } from 'semantic-ui-react';
+import { Header, Form, Grid, Segment, Message } from 'semantic-ui-react';
 
 const Login = function () {
   return (
     <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
       <Grid.Column style={{ maxWidth: 450 }}>
-        <Segment padded='very' stacked size='big'>
+        <Segment padded='very' stacked raised>
           <Header
             as='h2'
             content='¡Bienvenido a Safety Admin!'
@@ -20,9 +20,14 @@ const Login = function () {
               placeholder='Correo Electrónico'
             />
             <Form.Input
+              id='form-input-control-error-email'
               icon='lock'
               iconPosition='left'
               placeholder='Contraseña'
+              // error={{
+              //   content: 'Please enter a valid email address',
+              //   pointing: 'below'
+              // }}
             />
             <a>¿Olvidaste tu contraseña?</a>
             <Form.Button
@@ -34,6 +39,12 @@ const Login = function () {
             />
           </Form>
         </Segment>
+        <Message
+          error
+          icon='cancel'
+          iconPosition='left'
+          content='Incorrect username or password.'
+        />
       </Grid.Column>
     </Grid>
   );
